51af5b9728568e1dff9debdf82e2ee23583c6014
zhouwenli <1021092219.com>
2018-07-19 11:43:55 +0800

    登陆页面，input事件控制和click事件控制

--- src/components/index/Login.vue
+++ src/components/index/Login.vue
@@ -1,22 +1,46 @@
 <template>
 	<div class="page-login">
-		denglu
-		<p>{{logins}}</p>
+		<div class="loginHead">
+			登录
+		</div>
+		<div class="loginContent">
+			<img class="log" src="../../assets/img/login/login.png"/>
+		</div>
+		<div class="loginmess">
+			<p class="loginp"><label for="">号码</label>
+				<input type="text" name="" id="" value="" placeholder="请输入银盛通信手机号码"  v-model="number" />
+				<img v-bind:class="number==''?'':'active'" v-on:click="clear" src="../../assets/img/login/clear.png"/>
+			</p>
+			<p class="loginp"><label for="">密码</label>
+				<input type="text" name="" id="" value="" placeholder="请输入服务密码" v-model="password"/>
+				<img  class="padingLeft" src="../../assets/img/login/nosee.png"/>
+				<img src="../../assets/img/login/clear.png"/></p>
+		</div>
+		<div class="loginIn">
+			登录
+		</div>
 	</div>
 
 </template>
 <script>
-	export default {
+	export default{
 		name:'login',
 		data(){
-			return {
-				logins:'123123'
+			return{
+				number:'',
+				password:''
 			}
+		},
+		methods:{
+			clear(){
+				console.log(this.number)
+				this.number='';
+				console.log("1111111111")
+			}
+			
 		}
 	}
-
-
 </script>
-<style>
-	
+<style scoped>
+@import url("../../assets/css/login.css");
 </style>
