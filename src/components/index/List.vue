0fed87b010b3b5b9eb3e4bb4d0f52eafe09dd1a9
zhouwenli <1021092219.com>
2018-07-25 17:25:20 +0800

    业务中心

--- src/components/index/List.vue
+++ src/components/index/List.vue
@@ -1,8 +1,102 @@
 <template>
-	<div>
+	<div class="index-list">
 		
-		liebiaoaaaaaaaaaaaaaaaaaaaaa
-
+		<div class="businessContent">
+			<div class="imgBanner">
+				<img src="../../assets/img/list/businessbanner.jpg"/>
+			</div>
+		</div>
+		<div class="businessTab">
+			<div class="TabLeft">
+				<ul class="TabItem">
+					<li data-index="0" class="rgbaC active" ><span class="listBar"></span> 查询</li>
+					<li data-index="1"><span></span>选号</li>
+					<li data-index="2"><span></span>办理</li>
+					<li data-index="3"><span></span>缴费</li>
+					<li data-index="4"><span></span>客服</li>
+					<li data-index="5"><span></span>金融投资</li>
+					<li data-index="6"><span></span>物联网业务</li>
+					<li data-index="7"><span></span>帮助中心</li>
+					
+				</ul>
+			</div>
+			<div class="TabRight active">
+				<div class="Tabhr">
+					<hr class="hrL"/>
+					基础业务查询
+					<hr class="hrR" />
+				</div>
+				<ul class="Tabul">
+					<li>
+						<a href="#">
+							<img src="../../assets/img/list/checkIcon1.png"/>
+							<p>基本信息</p>
+						</a>
+						
+					</li>
+					<li>
+						<a href="#">
+							<img src="../../assets/img/list/checkIcon2.png"/>
+							<p>余量查询</p>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img src="../../assets/img/list/checkIcon3.png"/>
+							<p>话费余额</p>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img src="../../assets/img/list/checkIcon4.png"/>
+							<p>历史账单</p>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img src="../../assets/img/list/checkIcon5.png"/>
+							<p>充值记录</p>
+						</a>
+						
+					</li>
+					<li>
+						<a href="#">
+							<img src="../../assets/img/list/checkIcon6.png"/>
+							<p>资费专区</p>
+						</a>
+						
+					</li>
+					<li>
+						<a href="#">
+							<img src="../../assets/img/list/checkIcon7.png"/>
+							<p>已办业务</p>
+						</a>
+						
+					</li>
+					<li>
+						<a href="#">
+							<img src="../../assets/img/list/checkIcon8.png"/>
+							<p>订单查询</p>
+						</a>
+						
+					</li>
+					<li>
+						<a href="#">
+							<img src="../../assets/img/list/checkIcon9.png"/>
+							<p>发票查询</p>
+						</a>
+						
+					</li>
+					<li>
+						<a href="#">
+							<img src="../../assets/img/list/checkIcon10.png"/>
+							<p>详单查询</p>
+						</a>
+						
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
 
 
@@ -12,6 +106,6 @@
 
 	}
 </script>
-<style>
-
+<style scoped>
+@import url("../../assets/css/business_center.css");
 </style>
